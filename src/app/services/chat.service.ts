434e933084bb6688c3ba48621b69749f0927bba1
Jake Ruesink <jaruesink@gmail.com>
2016-10-13 14:57:39 -0500

    fixed flashing

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { Observable } from 'rxjs';
 import 'rxjs/Rx';
 
 @Injectable()
 export class ChatService {
+  chat_list: Array<any>;
   chat_list$: Observable<any>;
+  chat_senders$: any = {};
   items$: FirebaseListObservable<any>;
   constructor(public af: AngularFire) {
     this.items$ = af.database.list('/items', {
       query: { limitToLast: 15, orderByKey: true}
     });
     this.chat_list$ = this.items$.map(chats => {
-      console.log('chats: ', chats);
-      return chats.map(chat => {
-        if ( chat.sender !== af.database.object(`users/${chat.uid}`) ) {
-          chat.sender = af.database.object(`users/${chat.uid}`);
-        }
+      chats.forEach(chat => {
+        this.chat_senders$[chat.uid] = af.database.object(`users/${chat.uid}`);
+      });
+      return chats;
+    });
+    this.chat_list$.subscribe(chat_list => {
+      this.chat_list = chat_list.map(chat => {
+        this.chat_senders$[chat.uid].subscribe((sender) => {
+          chat.sender = sender;
+        });
         return chat;
       });
+      console.log(this.chat_list);
     });
   }
 }
